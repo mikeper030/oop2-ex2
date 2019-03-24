65fb904657e09f81cafcef2d6d3a48140e9d0bdb
Vizan Meir <vizanme@std.hac.ac.il>
2019-03-24 18:25:20 +0200

    update

--- ex2/Source.cpp
+++ ex2/Source.cpp
@@ -91,26 +91,26 @@ int main()
 	auto yearOfBirthField = std::make_unique<Field<int>>("What is your year of birth?");
 	auto destinationField = std::make_unique<Field<ValuesToNames<DestinationNames>>>("What is your flight destination?\n"
 		"(1 - Rome, 2 - Prague, 3 - New York, 4 - India, 5 - Thailand)");
-	/*auto classField = std::make_unique<Field<ValuesToNames<ClassNames>>>("Which class you want?\n"
+	auto classField = std::make_unique<Field<ValuesToNames<ClassNames>>>("Which class you want?\n"
 		"(1 - First Class, 2 - Business Class, 3 - Economy Class)");
 	auto currencyField = std::make_unique<Field<ValuesToNames<CurrencyNames>>>("In what currency will you pay?\n"
-		"(1 - NIS, 2 - Euro, 3 - Dollar)");*/
+		"(1 - NIS, 2 - Euro, 3 - Dollar)");
 
 	// Creating the field validators
 	auto nameValidator = std::make_unique<NoDigitValidator>();
 	auto idValidator = std::make_unique<IdValidator>();
 	auto ageValidator = std::make_unique<RangeValidator<int>>(currentYear() - MAX_AGE, currentYear() - MIN_AGE);
 	auto destinationValidator = std::make_unique<RangeValidator<ValuesToNames<DestinationNames>>>(1, 5);
-	/*auto classValidator = std::make_unique<RangeValidator<ValuesToNames<ClassNames>>>(1, 3);
-	auto currencyValidator = std::make_unique<RangeValidator<ValuesToNames<CurreSncyNames>>>(1, 3);
-*/
+	auto classValidator = std::make_unique<RangeValidator<ValuesToNames<ClassNames>>>(1, 3);
+	auto currencyValidator = std::make_unique<RangeValidator<ValuesToNames<CurrencyNames>>>(1, 3);
+
 	// Adding the validators to the fields
 	nameField->addValidetor(nameValidator.get());
 	idField->addValidetor(idValidator.get());
 	yearOfBirthField->addValidetor(ageValidator.get());     
 	destinationField->addValidetor(destinationValidator.get());
-	//classField->addValidator(classValidator.get());
-	//currencyField->addValidator(currencyValidator.get());
+	classField->addValidetor(classValidator.get());
+	currencyField->addValidetor(currencyValidator.get()); 
 
 	//// Creating form validators
 	//auto destinationToClassValidator
@@ -126,8 +126,8 @@ int main()
 	myForm.addField(idField.get());
 	myForm.addField(yearOfBirthField.get());
 	myForm.addField(destinationField.get());
-	//myForm.addField(classField.get());
-	//myForm.addField(currencyField.get());
+	myForm.addField(classField.get());
+	myForm.addField(currencyField.get());
 
 	//// Adding form validators
 	//myForm.addValidator(destinationToClassValidator.get());
